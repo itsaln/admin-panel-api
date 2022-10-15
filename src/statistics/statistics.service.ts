11e84d615f4b507ed6c0ea48bd59e3ecd57a31aa
itsaln <itsaln23@gmail.com>
2022-10-15 13:01:13 +0500

    05

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
+import { fn, col } from 'sequelize'
+import { IStatisticItem } from '@app/statistics/statistics.interface'
 import { ReviewModel } from '@app/review/review.model'
 import { MovieModel } from '@app/movie/movie.model'
 import { ViewsModel } from '@app/views/views.model'
-import { fn, col, literal } from 'sequelize'
 
 @Injectable()
 export class StatisticsService {
@@ -13,30 +14,47 @@ export class StatisticsService {
 		@InjectModel(ViewsModel) private readonly viewsModel: typeof ViewsModel
 	) {}
 
-	async getMainStatistics() {
+	async getMainStatistics(): Promise<IStatisticItem[]> {
 		const countReviews = await this.reviewModel.count()
 		const countMovies = await this.movieModel.count()
 
 		const views = await this.viewsModel.findAll({
 			attributes: [[fn('sum', col('views')), 'views']]
-		}).then(data => Number(data[0]?.views))
+		}).then(data => Number(data[0].views))
 
 		const averageRating = await this.movieModel.findAll({
 			attributes: [[fn('avg', col('rating')), 'rating']]
-		}).then(data => Number(data[0]?.rating.toFixed(1)))
+		}).then(data => Number(data[0].rating))
+		// .then(data => Number(data[0].rating.toFixed(1)))
 
-		return {
-			countReviews,
-			countMovies,
-			views,
-			averageRating
-		}
+		return [
+			{
+				id: 1,
+				name: 'Views',
+				value: views
+			},
+			{
+				id: 2,
+				name: 'Average rating',
+				value: averageRating
+			},
+			{
+				id: 3,
+				name: 'Movies',
+				value: countMovies
+			},
+			{
+				id: 4,
+				name: 'Reviews',
+				value: countReviews
+			}
+		]
 	}
 
 	async getMiddleStatistics() {
 		const totalFees = await this.movieModel.findAll({
 			attributes: [[fn('sum', col('fees')), 'fees']]
-		}).then(data => Number(data[0]?.fees))
+		}).then(data => Number(data[0].fees))
 
 		const viewsByMonth = await this.viewsModel.findAll({
 			attributes: [
