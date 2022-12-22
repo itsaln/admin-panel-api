d6f13c608af36f11cad196e9fb2650ec29a06909
itsaln <itsaln23@gmail.com>
2022-12-22 11:31:41 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@app/app.module'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: true })
 	app.setGlobalPrefix('api')
-	await app.listen(process.env.DB_PORT || 5000)
+	await app.listen(process.env.PORT || 5000)
+	console.log(`Application is running on: ${await app.getUrl()}`)
 }
 
 bootstrap()
